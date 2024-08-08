5abdb6cc94d4a2007c6b7e063ccb81da24aff31a
Alejandra2070 <am3793825@gmail.com>
2024-08-08 17:52:05 -0500

    :sparkles: Creación menú, archivos Json y arreglos README

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Farmacia
+
+Realización del trabajo asignado en clase.
+
 ## Descripción
+
+Se creó un menú para facilitar al usuario el uso del programa. Se podrán registrar las ventas y compras realizadas de manera sencilla y organizada.
+
+## Tecnologías utilizadas
+| Pyhton |
+|--|
+|![Tecnologías](image-1.png)|
+
+## Estructura
+
+![Estructura](image.png)
+
+## Características
+
+| Nombre | Descripción |
+|--|--|
+| | |
+
+## Instrucciones
+
+1. Copiar el enlace del repositorio
+
+![alt text](image-2.png)
+
+2. Clonar el repositorio
+
+![alt text](image-3.png)
+
+3. Abrir el archivo
+
+![alt text](<Captura de pantalla 2024-08-07 115918.png>)
+
+## Desarrollado por
+
+El trabajo fue desarrollado por Alejandra Machuca, estudiante de CampusLands Grupo T2.
---
+++ main.py
@@ -0,0 +1,154 @@
+#importamos date para que se agregue automaticamente la fecha cuando se solicita
+from datetime import date
+#importamos json para trabajar con el
+import json
+######################################
+
+#Hacemos definiciones e importamos los json
+def abrirArchivo():
+    miJson=[]
+    with open("medicamentos.json", encoding="utf-8") as openfile:
+        miJson= json.load(openfile)
+        return miJson
+archivo=abrirArchivo()
+
+def guardarArchivo(miData):
+    with open("medicamentos.json","w") as outfile:
+        json.dump(miData,outfile)
+#####################################
+def abrirArchivo2():
+    miJson2=[]
+    with open("ventas.json", encoding="utf-8") as openfile:
+        miJson2= json.load(openfile)
+        return miJson2
+archivo2=abrirArchivo2()
+
+def guardarArchivo2(miData):
+    with open("ventas.json","w") as outfile:
+        json.dump(miData,outfile)
+#####################################
+def abrirArchivo3():
+    miJson3=[]
+    with open("compras.json", encoding="utf-8") as openfile:
+        miJson3= json.load(openfile)
+        return miJson3
+archivo3=abrirArchivo3()
+
+def guardarArchivo3(miData):
+    with open("compras.json","w") as outfile:
+        json.dump(miData,outfile)
+######################################
+def abrirArchivo4():
+    miJson4=[]
+    with open("proveedores.json", encoding="utf-8") as openfile:
+        miJson4= json.load(openfile)
+        return miJson4
+archivo4=abrirArchivo4()
+
+def guardarArchivo4(miData):
+    with open("proveedores.json","w") as outfile:
+        json.dump(miData,outfile)
+######################################
+def abrirArchivo5():
+    miJson5=[]
+    with open("pacientes.json", encoding="utf-8") as openfile:
+        miJson5= json.load(openfile)
+        return miJson5
+archivo5=abrirArchivo5()
+
+def guardarArchivo5(miData):
+    with open("pacientes.json","w") as outfile:
+        json.dump(miData,outfile)
+######################################
+def abrirArchivo6():
+    miJson6=[]
+    with open("empleados.json", encoding="utf-8") as openfile:
+        miJson6= json.load(openfile)
+        return miJson6
+archivo6=abrirArchivo6()
+
+def guardarArchivo6(miData):
+    with open("empleados.json","w") as outfile:
+        json.dump(miData,outfile)
+######################################
+
+#creamos un menú para facilitar la realización del programa
+print("#################################")
+print("-------BIENVENIDOSSSSS!-------")
+print("#################################")
+print("-------------Menú-------------")
+print("""
+    1. Registrar ventas
+    2. Registrar compras
+    5. Salir del programa
+      """)
+
+#elegir cual de las opciones va a realizar
+opc=int(input("Elige una de las opciones de nuestro menú: "))
+miInfo=[]
+if (opc==1):
+    miInfo=abrirArchivo2() #abrimos la libreria
+    #ingresar la información de la venta
+    fechaR = str(input(date.today()))
+    nombreCR = input("Ingresa el nombre del paciente: ")
+    direccionR = int(input("Ingresa la dirección del paciente: "))
+    nombreER = input("Ingresa el nombre del empleado que realizó la venta: ")
+    cargoER = input("Ingresa el cargo del empleado que realizó la venta: ")
+    nombrePR = input("Ingresa el nombre del producto vendido: ")
+    cantidadPR = int(input("Ingresa la cantidad vendida del producto: "))
+    precioPR = int(input("Ingresa el precio del producto vendido: "))
+    #información para guardar la venta
+    registroR = {
+        "fechaVenta": fechaR,
+        "paciente":{
+            "nombre": nombreCR,
+            "direccion": direccionR,
+        },
+        "empleado":{
+            "nombre": nombreER,
+            "cargo": cargoER,
+        },
+        "medicamentosVendidos":{
+            "nombreMedicamento": nombrePR,
+            "cantidadVendida": cantidadPR,
+            "precio": precioPR
+        }
+    }
+    #abrir la libreria para guardar la información de la venta
+    miInfo = abrirArchivo2()
+    miInfo[0]["info"].append(registroR)
+    guardarArchivo2(miInfo)
+    #una vez guardada, se le informa al usuario
+    print("La venta se guardó correctamente.")
+elif(opc==2):
+    miInfo=abrirArchivo3() #abrir la libreria
+    #pedir la información de la compra
+    fechaPro = str(input(date.today()))
+    nombreProR = input("Ingresa el nombre del proveedor: ")
+    contactoPro = int(input("Ingresa el contacto del proveedor: "))
+    nombrePro = input("Ingresa el nombre del producto: ")
+    cantidadPro = int(input("Ingresa la cantidad del producto vendido: "))
+    precioPro = int(input("Ingresa el precio de la compra: "))
+    #información de la compra realizada
+    registro = {
+        "fechaCompra": fechaPro,
+        "proveedor":{
+            "nombre": nombreProR,
+            "contacto": contactoPro,
+        },
+        "medicamentosComprados":{
+            "nombreMedicamentos": nombrePro,
+            "cantidadComprada": cantidadPro,
+            "precioCompra": precioPro
+        } 
+    }
+    #abrir la libreria y guardar la información en ella
+    miInfo = abrirArchivo3()
+    miInfo[0]["compras"].append(registro)
+    guardarArchivo3(miInfo)
+    #se informa que la compra ya se guardó
+    print("La compra se realizó correctamente.")
+elif(opc==3):
+    print("Muchas gracias por utilizar nuestro programa. Vuelve pronto.")
+
+#Desarrollado por Alejandra Machuca Grupo T2
